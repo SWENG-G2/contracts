a2bb3fc5f7c0ece1575d6c0f95c5b17919888544
Giuseppe Barillari <joe2k01dev@gmail.com>
2023-02-12 20:16:50 +0000

    Javadocs

--- app/src/main/java/com/penelope/faunafinder/presentation/elements/PresentationElement.java
+++ app/src/main/java/com/penelope/faunafinder/presentation/elements/PresentationElement.java
@@ -5,13 +5,20 @@ import android.util.DisplayMetrics;
 import android.util.TypedValue;
 import android.widget.RelativeLayout;
 
-
+/**
+ * <code>PresentationElement</code> is the abstract class which provides
+ * all the methods and constants every presentation needs and must provide.
+ */
 public abstract class PresentationElement {
+    // Constants
     protected static final DisplayMetrics displayMetrics
             = Resources.getSystem().getDisplayMetrics();
     protected static final int MATCH_X_CLIENT_SIDE = -1;
     protected static final int MATCH_WIDTH_CLIENT_SIDE = -1;
     protected static final int ALIGN_CENTER_OF_PARENT = -2;
+    private static final int ALIGN_END_OF_PARENT = -3;
+    protected static final int MATCH_PARENT = -4;
+    protected static final int WRAP_CONTENT = -5;
 
     // View types
     public static final String AUDIO_ELEMENT = "audio";
@@ -19,7 +26,8 @@ public abstract class PresentationElement {
     public static final String TEXT_ELEMENT = "text";
     public static final String VIDEO_ELEMENT = "video";
 
-    private static final int ALIGN_END_OF_PARENT = -3;
+
+    // Requested screen position
     protected final int x, y;
 
     protected PresentationElement(int x, int y) {
@@ -27,15 +35,37 @@ public abstract class PresentationElement {
         this.y = y;
     }
 
+    /**
+     * Method an element provides to inform about the its type
+     *
+     * @return {@link #AUDIO_ELEMENT} or {@link #IMAGE_ELEMENT} or {@link #TEXT_ELEMENT} or {@link #VIDEO_ELEMENT}
+     */
     abstract public String getViewType();
 
+    /**
+     * Method used by the adapter to filter search queries.
+     *
+     * @return The text content of an element, or null if no searchable content should be provided.
+     */
     abstract public String getSearchableContent();
 
+    /**
+     * Utility used to convert DPs into pixels.
+     *
+     * @param input The value to convert in DPs.
+     * @return The converted value in pixels.
+     */
     protected int dpToPx(int input) {
         return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, input,
                 displayMetrics));
     }
 
+    /**
+     * Checks if layout rules are to be applied based on the requested screen position.
+     *
+     * @param layoutParams {@link RelativeLayout.LayoutParams} of the view being manipulated by the element.
+     * @return Boolean, whether no rules have been applied (true) or a rule has been applied (false).
+     */
     protected boolean noHorizontalLayoutRulesToApply(RelativeLayout.LayoutParams layoutParams) {
         switch (x) {
             case ALIGN_END_OF_PARENT:
--- app/src/main/java/com/penelope/faunafinder/presentation/elements/ViewElement.java
+++ app/src/main/java/com/penelope/faunafinder/presentation/elements/ViewElement.java
@@ -5,6 +5,19 @@ import android.view.ViewGroup;
 
 import com.penelope.faunafinder.xml.slide.Slide;
 
+/**
+ * Any class extending {@link PresentationElement} and handling a class which extends {@link View}
+ * must implement this interface.
+ */
 public interface ViewElement {
+    /**
+     * Method called by the ViewHolder to apply view manipulations.
+     *
+     * @param parent    The View containing the element, usually {@link android.widget.RelativeLayout}
+     * @param container The ViewGroup attached to the ViewHolder.
+     * @param slide     The slide the element belongs to.
+     * @param id        The ID of the View the element is going to manipulate.
+     * @return The manipulated View.
+     */
     View applyView(View parent, ViewGroup container, Slide slide, int id);
 }
