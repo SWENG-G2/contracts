fe4446d2c558cbd85b55b5de90763417001e1409
ssophiemaw <97915175+ssophiemaw@users.noreply.github.com>
2023-03-08 17:02:55 +0000

    Tests

--- app/src/test/java/com/penelope/faunafinder/presentation/elements/TextElementUnitTest.java
+++ app/src/test/java/com/penelope/faunafinder/presentation/elements/TextElementUnitTest.java
@@ -1,8 +1,11 @@
 package com.penelope.faunafinder.presentation.elements;
 
+import static com.penelope.faunafinder.presentation.elements.PresentationElement.displayMetrics;
+
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
@@ -11,6 +14,7 @@ import android.widget.TextView;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import com.penelope.faunafinder.R;
 import com.penelope.faunafinder.xml.slide.BasicSlide;
 
 import org.junit.Assert;
@@ -23,7 +27,12 @@ import org.robolectric.annotation.Implements;
 import java.util.ArrayList;
 
 /*
-no idea how to get "RelativeLayout parent = findViewById(R.id.activity_main)" in this
+Tests I need:
+size: width(done), height(done), font size(done), margin params (x and y pos)(done)
+font(done)
+font color(done)
+time on screen
+text on view
  */
 @RunWith(RobolectricTestRunner.class)
 @Implements(ResourcesCompat.class)
@@ -31,17 +40,7 @@ public class TextElementUnitTest{
 
    RelativeLayout parent;
     BasicSlide basicSlide;
-
-
    int ELEMENT_ID = 8008135;
-
-   private View getViewOfTextElementWithWH(int width, int height){
-       TextElement textElement = new TextElement(null, 32, Color.BLACK, 100, 500, width, height, 5000L);
-       textElement.setContent("Test input");
-       View test= textElement.applyView(parent,(ViewGroup) parent,basicSlide,ELEMENT_ID);
-
-       return test;
-   }
 
     @Before
     public void setUpTest() {
@@ -51,81 +50,58 @@ public class TextElementUnitTest{
 
     }
     @Test
-    public void positiveWidthSetCorrectly(){
-        int width=1800;
-        int height= 25;
-        View testView= getViewOfTextElementWithWH(width,height);
-        float expectedWidth= width* basicSlide.getCalculatedWidth()/(float) basicSlide.getWidth();
-        float delta= 0f;
-        int actualWidth= testView.getLayoutParams().width;
+    public void widthIsSetCorrectly(){
+       int height= 100;
+       ArrayList testWidths= new ArrayList<Integer>();
+       testWidths.add(100);
+       testWidths.add(-1);
+       testWidths.add(-2);
 
-        Assert.assertEquals( expectedWidth, actualWidth,delta);
+        ArrayList expectedWidths= new ArrayList<Integer>();
+        expectedWidths.add(Math.round(100* basicSlide.getCalculatedWidth()/(float) basicSlide.getWidth()));
+        expectedWidths.add(RelativeLayout.LayoutParams.MATCH_PARENT);
+        expectedWidths.add(RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        for(int i=0;i<3;i++){
+            TextElement textElement = new TextElement(null, 32, Color.BLACK, 100, 500, (int)testWidths.get(i), height, 5000L);
+            textElement.setContent("Test input");
+            View testView= textElement.applyView(parent,(ViewGroup) parent,basicSlide,ELEMENT_ID);
+            int actualWidth= testView.getLayoutParams().width;
+            Assert.assertEquals(expectedWidths.get(i), actualWidth);
+        }
     }
     @Test
-    public void positiveHeightSetCorrectly(){
-        int height=25;
-        TextElement textElement = new TextElement(null, 32, Color.BLACK, 100, 500, 1800, height, 5000L);
-        textElement.setContent("Test input");
-        View test= textElement.applyView(parent,(ViewGroup) parent,basicSlide,ELEMENT_ID);
-        float expectedHeight= textElement.dpToPx(height);
-        float delta= 0f;
-        int actualHeight= test.getLayoutParams().height;
-        Assert.assertEquals( expectedHeight, actualHeight,delta);
+    public void heightIsSetCorrectly(){
+        int width= 100;
+        ArrayList testHeights= new ArrayList<Integer>();
+        testHeights.add(100);
+        testHeights.add(-1);
+        testHeights.add(-2);
+
+        ArrayList expectedHeights= new ArrayList<Integer>();
+        expectedHeights.add(Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100, displayMetrics)));
+        expectedHeights.add(RelativeLayout.LayoutParams.MATCH_PARENT);
+        expectedHeights.add(RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        for(int i=0;i<3;i++){
+            TextElement textElement = new TextElement(null, 32, Color.BLACK, 100, 500,width, (int)testHeights.get(i), 5000L);
+            textElement.setContent("Test input");
+            View testView= textElement.applyView(parent,(ViewGroup) parent,basicSlide,ELEMENT_ID);
+            int actualHeight= testView.getLayoutParams().height;
+            Assert.assertEquals(expectedHeights.get(i), actualHeight);
+        }
     }
     @Test
-    public void matchParentHeightSetCorrectly() throws NoSuchFieldException, IllegalAccessException {
-        int height=-1;
-        TextElement textElement = new TextElement(null, 32, Color.BLACK, 100, 500, 1800, height, 5000L);
-        textElement.setContent("Test input");
-        View test= textElement.applyView(parent,(ViewGroup) parent,basicSlide,ELEMENT_ID);
-        float expectedHeight= RelativeLayout.LayoutParams.MATCH_PARENT;
-        float delta= 0f;
-        int actualHeight= test.getLayoutParams().height;
-        Assert.assertEquals( expectedHeight, actualHeight,delta);
-    }
-    @Test
-    public void wrapContentHeightSetCorrectly() throws NoSuchFieldException, IllegalAccessException {
-        int height=-2;
-        TextElement textElement = new TextElement(null, 32, Color.BLACK, 100, 500, 1800, height, 5000L);
-        textElement.setContent("Test input");
-        View test= textElement.applyView(parent,(ViewGroup) parent,basicSlide,ELEMENT_ID);
-        int expectedHeight= RelativeLayout.LayoutParams.WRAP_CONTENT;
-        float delta= 0f;
-        int actualHeight= test.getLayoutParams().height;
-        Assert.assertEquals( expectedHeight, actualHeight,delta);
-    }
-    @Test
-    public void matchParentWidthSetCorrectly() throws NoSuchFieldException, IllegalAccessException {
-        int width=-1;
-        View testView= getViewOfTextElementWithWH(width,25);
-        int expectedWidth= RelativeLayout.LayoutParams.MATCH_PARENT;
-        float delta= 0f;
-        int actualWidth=testView.getLayoutParams().width;
-        Assert.assertEquals( expectedWidth,actualWidth,delta);
-    }
-    @Test
-    public void wrapContentWidthSetCorrectly() throws NoSuchFieldException, IllegalAccessException {
-        int width=-2;
-        View testView= getViewOfTextElementWithWH(width,25);
-        int expectedWidth= RelativeLayout.LayoutParams.WRAP_CONTENT;
-        float delta= 0f;
-        int actualWidth=testView.getLayoutParams().width;
-        Assert.assertEquals( expectedWidth,actualWidth,delta);
-    }
-    @Test
-    public void fontIsSetCorrectly() throws NoSuchFieldException, IllegalAccessException {
+    public void fontIsSetCorrectly() {
        ArrayList fonts= new ArrayList<String>();
        fonts.add("null");
-      // fonts.add("mono");
-      // fonts.add("roberto");
+       fonts.add("mono");
+       fonts.add("roberto");
 
        ArrayList typefaces= new ArrayList<Typeface>();
        typefaces.add(Typeface.DEFAULT);
-       //typefaces.add(ResourcesCompat.getFont(parent.getContext(),font.chivo_mono_regular ));
-       //typefaces.add(ResourcesCompat.getFont(parent.getContext(),font.roboto_condensed_regular));
-    /*
-       its giving a Resources$NotFoundException when these are uncommented
-     */
+       typefaces.add(ResourcesCompat.getFont(parent.getContext(), R.font.chivo_mono_regular));
+       typefaces.add(ResourcesCompat.getFont(parent.getContext(),  R.font.chivo_mono_regular));
 
        for(int i=0;i<1;i++){
            TextElement textElement = new TextElement((String)fonts.get(i), 32, Color.BLACK, 100, 500, 1800, 25, 5000L);
@@ -133,5 +109,57 @@ public class TextElementUnitTest{
            View test= textElement.applyView(parent,(ViewGroup) parent,basicSlide,ELEMENT_ID);
            Assert.assertEquals(TextView.class.cast(test).getTypeface().toString(),((Typeface)typefaces.get(i)).toString());
        }
+    }
+    @Test
+    public void fontColorIsCorrect(){
+        ArrayList expectedColors= new ArrayList<Integer>();
+        expectedColors.add(Color.BLACK);
+        expectedColors.add(Color.WHITE);
+        expectedColors.add(Color.rgb(200,200,200));
+        for(int i=0;i<expectedColors.size();i++){
+            TextElement textElement = new TextElement(null, 32, (int)expectedColors.get(i), 100, 500, 500, 25, 5000L);
+            textElement.setContent("Test input");
+            TextView testView= TextView.class.cast(textElement.applyView(parent,(ViewGroup) parent,basicSlide,ELEMENT_ID));
+            int actualColor= testView.getCurrentTextColor();
+            Assert.assertEquals(expectedColors.get(i), actualColor);
+        }
+    }
+    @Test
+    public void xyParamsSetCorrectly(){
+        ArrayList testXYPositions= new ArrayList<Integer>();
+        testXYPositions.add(5); //x
+        testXYPositions.add(5); //y
+        testXYPositions.add(2); //x
+        testXYPositions.add(8); //y
+        testXYPositions.add(100); //x
+        testXYPositions.add(200); //y
+
+        for(int i=0;i<testXYPositions.size();i=i+2){
+            TextElement textElement = new TextElement(null, 32, Color.BLACK, (int)testXYPositions.get(i),(int)testXYPositions.get(i+1), 1000, 25, 5000L);
+            textElement.setContent("Test input");
+            TextView testView= TextView.class.cast(textElement.applyView(parent,(ViewGroup) parent,basicSlide,ELEMENT_ID));
+            ViewGroup.MarginLayoutParams mlp= (ViewGroup.MarginLayoutParams) testView.getLayoutParams();
+            int actualX= mlp.leftMargin;
+            int actualY= mlp.topMargin;
+            int expectedX=  Math.round((((int)testXYPositions.get(i)) * basicSlide.getCalculatedWidth()) / (float) basicSlide.getWidth());
+            int expectedY= Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, (int)testXYPositions.get(i+1), displayMetrics));
+            Assert.assertEquals(expectedX, actualX);
+            Assert.assertEquals(expectedY, actualY);
+        }
+    }
+    @Test
+    public void fontSizeIsSetCorrectly(){
+        ArrayList testFontSizes= new ArrayList<Integer>();
+        testFontSizes.add(1);
+        testFontSizes.add(5);
+        testFontSizes.add(10);
+        for(int i=0;i<testFontSizes.size();i++) {
+            TextElement textElement = new TextElement(null, (int) testFontSizes.get(i), Color.BLACK, 100, 100, 1000, 25, 5000L);
+            textElement.setContent("Test input");
+            TextView testView = TextView.class.cast(textElement.applyView(parent, (ViewGroup) parent, basicSlide, ELEMENT_ID));
+            int expectedFontSize = View.getDefaultSize((int) testFontSizes.get(i), TypedValue.COMPLEX_UNIT_SP);
+            int actualFontSize = Math.round(testView.getTextSize());
+            Assert.assertEquals(expectedFontSize, actualFontSize);
+        }
     }
 }
