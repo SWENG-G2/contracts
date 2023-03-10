fdafcfd632074d3b8d6087089d55c4ffeb209782
fltodd <fraser.todd.501@gmail.com>
2023-03-10 21:48:15 +0000

    finished testing and removed additional handlers

---
+++ app/src/test/java/com/penelope/faunafinder/presentation/elements/ImageElementTest.java
@@ -0,0 +1,14 @@
+package com.penelope.faunafinder.presentation.elements;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class ImageElementTest {
+
+    @Test
+    public void clickingLogin_shouldStartLoginActivity() {
+        System.out.println("Yah");
+    }
+}
